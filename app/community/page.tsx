2688a545df3d31dd3af482b4883001e434f51796
santiaprl <aprilsantiaggo22@gmail.com>
2025-06-07 13:22:32 -0700

    Typo fix

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -309,7 +309,7 @@ export default function CommunityPage() {
         </div>
       
       <p className="italic text-xs text-center mt-5 text-gray-900 max-w-sm mx-auto">
-        Private spaces are coming soon—one for each emotional arc. Not everyone will be allowed in.
+        Private spaces are coming soon—one for each emotional arc.
         But if this arc speaks to where you are right now,{" "}
         <a
           href="https://forms.gle/NVEGTg8gmonLmxxW7"
