8c8ad9e93e5234e779d9c4a6069d8795d08f36f7
santiaprl <mommamusic100@gmail.com>
2025-08-16 12:50:24 -0700

    Updated Prices of new and current listings

--- app/books/the-daily-stoic/page.tsx
+++ app/books/the-daily-stoic/page.tsx
@@ -77,7 +77,7 @@ export default function ProductPage() {
           <div className="flex flex-col space-y-8">
             <div className="space-y-2">
               <h1 className="text-3xl md:text-4xl font-light tracking-tight">The Daily Stoic</h1>
-              <p className="text-2xl font-light">$130.00</p>
+              <p className="text-2xl font-light">$125.00</p>
             </div>
 
             <div className="h-px bg-neutral-200" />
